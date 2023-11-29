d2df905ea36c016447e564a2467b43b3a92fc6db
jihyeon.kim <jihyeon.kim@hnine.com>
2023-11-29 14:31:21 +0900

    threeJourney - 연습용파일

---
+++ threejs_journey/exercise/script.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+//canvas
+const canvas = document.querySelector('canvas.webgl');
+
+//Scene
+const scene = new THREE.Scene();
+
+//Geometry
+//para - w h d
+const geometry = new THREE.BoxGeometry(1,1,1);
+
+//material
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const Mesh = new THREE.Mesh(geometry, material);
+scene.add(Mesh);
+
+//size
+const sizes = {
+  width: 800,
+  height: 600
+}
+
+//camera
+//point of view
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
