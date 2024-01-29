c2ef73c33da7fb2247db6f6f4fb57048b656d32b
jihyeon.kim <jihyeon.kim@hnine.com>
2024-01-29 15:58:03 +0900

    API함수를 이용해서 데이터 받아오기

--- nextjs/first-next-js/src/app/products/[slug]/page.tsx
+++ nextjs/first-next-js/src/app/products/[slug]/page.tsx
@@ -1,5 +1,7 @@
 // Import necessary dependencies and components
+import { getProduct, getProducts } from "@/service/products";
 import ProductNotFound from "../not-found";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: {
@@ -11,20 +13,24 @@ export function generateMetadata({ params }: Props) {
   return { title: `제품의 이름: ${params.slug}` };
 }
 
-function PantsPage({ params }: Props) {
-  if (params.slug === 'nothing') {
-    return <ProductNotFound />;
+function PantsPage({ params: {slug} }: Props) {
+  const product = getProduct(slug);
+
+  if(!product) {
+    notFound();
   }
 
   return (
+    //서버 파일에 있는 데이트중 해당 제품의 정보를 찾아서 그걸 보여줌
     <div>
-      <div>{params.slug} 설명 디테일 페이지</div>
+      <div>{product} 설명 디테일 페이지</div>
     </div>
   );
 }
 
 export function generateStaticParams() {
-  const products = ['pants', 'skirt'];
+  //모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임(ssg)
+  const products = getProducts();
   return products.map((product) => ({
     slug: product,
   }));
--- nextjs/first-next-js/src/app/products/page.tsx
+++ nextjs/first-next-js/src/app/products/page.tsx
@@ -1,8 +1,10 @@
+import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-const products = ['shirt', 'pants', 'skirt', 'shoes'];
+const products = getProducts();
 
 export default function ProductPage() {
+  //서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌)
   return (
     <>
       <div>제품설명디테일페이지</div>
