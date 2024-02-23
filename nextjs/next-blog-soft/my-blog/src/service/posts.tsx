67b948ad2055a46ef432e20ca16d4436de4176af
jihyeon.kim <jihyeon.kim@hnine.com>
2024-02-23 15:29:24 +0900

    블로그 마크다운 포스팅  작업 - 마크다운 데이터가져오는 함수 추가

--- nextjs/next-blog-soft/my-blog/src/service/posts.tsx
+++ nextjs/next-blog-soft/my-blog/src/service/posts.tsx
@@ -2,6 +2,7 @@ import { readFile } from "fs/promises";
 import path from "path";
 
 
+
 export type Post = {
   title: string;
   description: string;
@@ -9,7 +10,9 @@ export type Post = {
   category: string;
   path: string;
   featured: boolean;
-}
+};
+
+export type PostData = Post & {content:string};
 
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts()
@@ -27,4 +30,19 @@ export async function getAllPosts(): Promise<Post[]> {
   .then<Post[]>(JSON.parse)
   .then((posts) => posts.sort((a,b) => (a.date> b.date ? -1 : 1)));
 
+}
+
+
+//함수를 만들고, 파일 이름을 전달해야하는데 
+//promise비동기 함수고
+//전달하는 타입은 Psot는 일반 메타데이터 정보를 가지고 있었고
+//그 컨텐츠가 들어있는 PostData라는타입을 반환해주도록 한다.
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filepath = path.join(process.cwd(), 'data', 'posts',`${fileName}.md`);
+  const metadata = await getAllPosts() //
+  .then((posts) => posts.find((post) => post.path === fileName));
+  if(!metadata) throw new Error(`$(fileName)에 해당하는 포스트를 찾을 수 없음`);
+
+  const content = await readFile(filepath, 'utf-8');
+  return{...metadata, content};
 }
