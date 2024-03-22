b68f4b6ab2332e3225c7bc28f7718e1fb1f40d0a
jihyeon.kim <jihyeon.kim@hnine.com>
2024-03-22 18:30:20 +0900

    3d오브젝트 테스트

--- jihyeon/vite-project/src/script.js
+++ jihyeon/vite-project/src/script.js
@@ -1,76 +1,126 @@
-import gsap from "gsap";
-import { CustomEase } from "gsap/all";
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import GUI from 'lil-gui'
+import gsap from 'gsap'
 
+/**
+ * Debug
+ */
+const gui = new GUI()
 
-CustomEase.create("cubic", "0.83, 0, 0.17, 1" );
-let isAnimating = false;
-
-function splitTextIntoSpans(selector) {
-  let elements = document.querySelectorAll(selector);
-
-  elements.forEach( (element)=>{
-    let text = element.innerText;
-    let splitText = text.split("").map(function(char){
-      return `<span>${char === "" ? "&nbsp;&nbsp;" : char}</span>`
-    }).join("");
-    element.innerHTML = splitText;
-  });
+const parameters = {
+  materialColor: '#ffeded'
 }
 
-function initializeCards(){
-  let cards = Array.from(document.querySelectorAll('.card'));
-  gsap.to( cards, {
-     y: (i) => -15 + 15 * i + "%",
-     z: (i) => 15 * i,
-     duration: 1,
-     ease: 'cubic',
-     stagger: -0.1,
-  });
+gui
+  .addColor(parameters, 'materialColor')
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('./draco/')
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2)
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  splitTextIntoSpans('.copy h1');
-  initializeCards();
+window.addEventListener('resize', () =>
+{
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-  gsap.set('h1 span', {y: -200});
-  gsap.set('.slider .card:last-child h1 span', {y:0});
-});
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-document.addEventListener('click', function(){
-  if(isAnimating) return;
-
-  isAnimating = true;
-
-  let slider = document.querySelector('.slider');
-  let cards = Array.from(slider.querySelectorAll('.card'));
-  let lastCard = cards.pop();
-  let nextCard = cards[cards.length - 1];
-
-  gsap.to(lastCard.querySelectorAll('h1 span'), {
-    y: 200,
-    duration: 0.75,
-    ease: 'cubic'
-  });
-
-  gsap.to(lastCard, {
-    y: "+=150%",
-    duration: 0.75,
-    ease: 'cubic',
-    onComplete: () => {
-      slider.prepend(lastCard);
-      initializeCards();
-      gsap.set(lastCard.querySelectorAll('h1 span'), {y: -200});
-
-      setTimeout( ()=> {
-        isAnimating = false;
-      },1000 )
-    },
-  });
-
-  gsap.to(nextCard.querySelectorAll('h1 span'), {
-    y: 0,
-    duration: 1,
-    ease: 'cubic',
-    stagger: 0.05
-  })
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+
+/**
+ * plane
+ */
+const FogColor = 0x004fff;
+const FloorColor = 0x555555;
+const planeGeometry = new THREE.PlaneGeometry(30,30,1,1)
+const planeMaterial = new THREE.MeshStandardMaterial({
+  color: 0x55555
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+plane.rotation.x = -0.5 * Math.PI
+plane.position.y= -0.5
+scene.add(plane)
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+scene.add(camera)
+
+/**
+ * Load
+ */
+gltfLoader.load('./letter.glb', (gltf) => {
+
+  const characterMesh = gltf.scene.children[0];
+
+  scene.add(characterMesh )
+
+  characterMesh.scale.set(10,10,10);
+})
+
+/**
+ * Lights
+ */
+const spotLight = new THREE.SpotLight( 0xffffde, 200 );
+spotLight.position.set( 3.5, 0, 7 );
+scene.add( spotLight );
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+  const elapsedTime = clock.getElapsedTime()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
