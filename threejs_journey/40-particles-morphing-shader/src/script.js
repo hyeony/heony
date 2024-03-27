2437cb0181aa5f380e3c25ded3417448158b5efa
jihyeon.kim <jihyeon.kim@hnine.com>
2024-03-27 15:49:10 +0900

    perticle.position map함수 활용 코드 추가

--- threejs_journey/40-particles-morphing-shader/src/script.js
+++ threejs_journey/40-particles-morphing-shader/src/script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import {  DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
 import GUI from 'lil-gui'
 import gsap from 'gsap'
 import particlesVertexShader from './shaders/particles/vertex.glsl'
@@ -11,7 +11,9 @@ import particlesFragmentShader from './shaders/particles/fragment.glsl'
  * Base
  */
 // Debug
-const gui = new GUI({ width: 340 })
+const gui = new GUI({
+  width: 340
+})
 const debugObject = {}
 
 // Canvas
@@ -35,15 +37,14 @@ const sizes = {
   pixelRatio: Math.min(window.devicePixelRatio, 2)
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
   // Materials
-  if(particles)
+  if (particles)
     particles.material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
   // Update camera
@@ -79,12 +80,14 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
 
 debugObject.clearColor = '#160920'
-gui.addColor(debugObject, 'clearColor').onChange(() => { renderer.setClearColor(debugObject.clearColor) })
+gui.addColor(debugObject, 'clearColor').onChange(() => {
+  renderer.setClearColor(debugObject.clearColor)
+})
 renderer.setClearColor(debugObject.clearColor)
 
 
 //Load models
-let particles= null
+let particles = null
 gltfLoader.load('./models.glb', (gltf) => {
   // console.log(gltf)
   /**
@@ -94,50 +97,43 @@ gltfLoader.load('./models.glb', (gltf) => {
   particles.index = 0
 
   //Positions
-  const positions =  gltf.scene.children.map( child => child.geometry.attributes.position)
+  const positions = gltf.scene.children.map(child => child.geometry.attributes.position)
 
   particles.maxCount = 0
-  for( const position of positions )
-  {
-    if(position.count > particles.maxCount)
+  for (const position of positions) {
+    if (position.count > particles.maxCount)
       particles.maxCount = position.count
   }
 
-  particles.positions = []
-  for(const position of positions)
-  {
+  particles.positions = positions.map( position => {
     const originalArray = position.array
     const newArray = new Float32Array(particles.maxCount * 3)
 
-    for (let i = 0; i < particles.maxCount; i ++)
-    {
+    for (let i = 0; i < particles.maxCount; i++) {
       const i3 = i * 3
 
-      if(i3 < originalArray.length)
-      {
+      if (i3 < originalArray.length) {
         newArray[i3 + 0] = originalArray[i3 + 0]
         newArray[i3 + 1] = originalArray[i3 + 1]
         newArray[i3 + 2] = originalArray[i3 + 2]
-      }
-      else
-      {
-        const randomIndex =  Math.floor(position.count * Math.random()) * 3
-        // console.log(randomIndex)
+      } else {
+        const randomIndex = Math.floor(position.count * Math.random()) * 3
         newArray[i3 + 0] = originalArray[randomIndex + 0]
         newArray[i3 + 1] = originalArray[randomIndex + 1]
         newArray[i3 + 2] = originalArray[randomIndex + 2]
       }
     }
 
-    particles.positions.push(new THREE.Float32BufferAttribute(newArray, 3))
+    return new THREE.Float32BufferAttribute(newArray, 3)
+  })
 
-  }
+
   // console.log(particles.positions)
   const sizesArray = new Float32Array(particles.maxCount)
 
-  for(let i = 0; i < particles.maxCount; i++)
-  sizesArray[i] = Math.random()
-// console.log(sizesArray)
+  for (let i = 0; i < particles.maxCount; i++)
+    sizesArray[i] = Math.random()
+  // console.log(sizesArray)
 
   // Geometry
   particles.geometry = new THREE.BufferGeometry()
@@ -153,13 +149,12 @@ gltfLoader.load('./models.glb', (gltf) => {
   particles.material = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
     fragmentShader: particlesFragmentShader,
-    uniforms:
-    {
-        uSize: new THREE.Uniform(0.4),
-        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
-        uProgress: new THREE.Uniform(0),
-        uColorA: new THREE.Uniform(new THREE.Color(particles.colorA)),
-        uColorB: new THREE.Uniform(new THREE.Color(particles.colorB))
+    uniforms: {
+      uSize: new THREE.Uniform(0.4),
+      uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+      uProgress: new THREE.Uniform(0),
+      uColorA: new THREE.Uniform(new THREE.Color(particles.colorA)),
+      uColorB: new THREE.Uniform(new THREE.Color(particles.colorB))
     },
     blending: THREE.AdditiveBlending,
     depthWrite: false
@@ -179,27 +174,43 @@ gltfLoader.load('./models.glb', (gltf) => {
 
     //Animate uProgress
     gsap.fromTo(
-      particles.material.uniforms.uProgress,
-      { value: 0 },
-      { value: 1, duration: 3, ease: 'linear' }
+      particles.material.uniforms.uProgress, {
+        value: 0
+      }, {
+        value: 1,
+        duration: 3,
+        ease: 'linear'
+      }
     )
 
     //Save index
     particles.index = index
   }
 
-  particles.morph0 = () => {particles.morph(0) }
-  particles.morph1 = () => {particles.morph(1) }
-  particles.morph2 = () => {particles.morph(2) }
-  particles.morph3 = () => {particles.morph(3) }
+  particles.morph0 = () => {
+    particles.morph(0)
+  }
+  particles.morph1 = () => {
+    particles.morph(1)
+  }
+  particles.morph2 = () => {
+    particles.morph(2)
+  }
+  particles.morph3 = () => {
+    particles.morph(3)
+  }
 
 
   // Tweaks
-  gui.add (particles.material.uniforms.uProgress, 'value')
-  .min(0).max(1).step(0.001).name('uProgress')
+  gui.add(particles.material.uniforms.uProgress, 'value')
+    .min(0).max(1).step(0.001).name('uProgress')
 
-  gui.addColor(particles, 'colorA').onChange(() => { particles.material.uniforms.uColorA.value.set(particles.colorA) })
-  gui.addColor(particles, 'colorB').onChange(() => { particles.material.uniforms.uColorB.value.set(particles.colorB) })
+  gui.addColor(particles, 'colorA').onChange(() => {
+    particles.material.uniforms.uColorA.value.set(particles.colorA)
+  })
+  gui.addColor(particles, 'colorB').onChange(() => {
+    particles.material.uniforms.uColorB.value.set(particles.colorB)
+  })
 
   gui.add(particles, 'morph0')
   gui.add(particles, 'morph1')
@@ -214,8 +225,7 @@ gltfLoader.load('./models.glb', (gltf) => {
 /**
  * Animate
  */
-const tick = () =>
-{
+const tick = () => {
   // Update controls
   controls.update()
 
