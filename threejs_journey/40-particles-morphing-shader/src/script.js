ca0f3f58e1d82ff4be51135a206a17de7f88b743
jihyeon.kim <jihyeon.kim@hnine.com>
2024-03-29 18:14:10 +0900

    포지션옮기기 진행중

--- threejs_journey/40-particles-morphing-shader/src/script.js
+++ threejs_journey/40-particles-morphing-shader/src/script.js
@@ -62,7 +62,8 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0, 8 * 2)
+camera.position.set(0, 0, 10)
+
 scene.add(camera)
 
 // Controls
@@ -86,192 +87,341 @@ gui.addColor(debugObject, 'clearColor').onChange(() => {
 })
 renderer.setClearColor(debugObject.clearColor)
 
+let particlesAmongUs = null;
+let particlesRocket = null;
 
-//Load models
-let particles = null
-gltfLoader.load('./models.glb', (gltf) => {
-  // console.log(gltf)
-  /**
-   * Particles
-   */
-  const particles = {}
-  particles.index = 0
 
-  //Positions
-  const positions = gltf.scene.children.map(child => child.geometry.attributes.position)
+gltfLoader.load('./among_us.glb', (gltf) => {
+  particlesAmongUs = {};
+  particlesAmongUs.index = 0;
+  gltf.scene.position.x = 10;
 
-  particles.maxCount = 0
+  const positions = gltf.scene.children.map(child => child.geometry.attributes.position);
+
+  particlesAmongUs.maxCount = 0;
+
   for (const position of positions) {
-    if (position.count > particles.maxCount)
-      particles.maxCount = position.count
+    if (position.count > particlesAmongUs.maxCount)
+      particlesAmongUs.maxCount = position.count;
   }
 
-  particles.positions = positions.map( position => {
-    const originalArray = position.array
-    const newArray = new Float32Array(particles.maxCount * 3)
+  particlesAmongUs.positions = positions.map(position => {
+    const originalArray = position.array;
+    const newArray = new Float32Array(particlesAmongUs.maxCount * 3);
 
-    for (let i = 0; i < particles.maxCount; i++) {
-      const i3 = i * 3
+    for (let i = 0; i < particlesAmongUs.maxCount; i++) {
+      const i3 = i * 3;
 
       if (i3 < originalArray.length) {
-        newArray[i3 + 0] = originalArray[i3 + 0]
-        newArray[i3 + 1] = originalArray[i3 + 1]
-        newArray[i3 + 2] = originalArray[i3 + 2]
+        newArray[i3 + 0] = originalArray[i3 + 0];
+        newArray[i3 + 1] = originalArray[i3 + 1];
+        newArray[i3 + 2] = originalArray[i3 + 2];
       } else {
-        const randomIndex = Math.floor(position.count * Math.random()) * 3
-        newArray[i3 + 0] = originalArray[randomIndex + 0]
-        newArray[i3 + 1] = originalArray[randomIndex + 1]
-        newArray[i3 + 2] = originalArray[randomIndex + 2]
+        const randomIndex = Math.floor(position.count * Math.random()) * 3;
+        newArray[i3 + 0] = originalArray[randomIndex + 0];
+        newArray[i3 + 1] = originalArray[randomIndex + 1];
+        newArray[i3 + 2] = originalArray[randomIndex + 2];
       }
     }
 
-    return new THREE.Float32BufferAttribute(newArray, 3)
-  })
+    return new THREE.Float32BufferAttribute(newArray, 3);
+  });
 
+  const sizesArray = new Float32Array(particlesAmongUs.maxCount);
 
-  // console.log(particles.positions)
-  const sizesArray = new Float32Array(particles.maxCount)
-
-  for (let i = 0; i < particles.maxCount; i++)
-    sizesArray[i] = Math.random()
-  // console.log(sizesArray)
+  for (let i = 0; i < particlesAmongUs.maxCount; i++)
+    sizesArray[i] = Math.random();
 
   // Geometry
-  particles.geometry = new THREE.BufferGeometry()
-  particles.geometry.setAttribute('position', particles.positions[particles.index])
-  particles.geometry.setAttribute('aPositionTarget', particles.positions[2])
-  particles.geometry.setAttribute('aSize', new THREE.BufferAttribute(sizesArray, 1))
-  //index의 제거방법에대해 기억...
-
+  particlesAmongUs.geometry = new THREE.BufferGeometry();
+  particlesAmongUs.geometry.setAttribute('position', particlesAmongUs.positions[particlesAmongUs.index]);
+  particlesAmongUs.geometry.setAttribute('aPositionTarget', particlesAmongUs.positions[0]);
+  particlesAmongUs.geometry.setAttribute('aSize', new THREE.BufferAttribute(sizesArray, 1));
 
   // Material
-  particles.colorA = '#ff7300'
-  particles.colorB = '#0091ff'
-  particles.material = new THREE.ShaderMaterial({
+  particlesAmongUs.colorA = '#ff7300';
+  particlesAmongUs.colorB = '#0091ff';
+  particlesAmongUs.material = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
     fragmentShader: particlesFragmentShader,
     uniforms: {
       uSize: new THREE.Uniform(0.4),
       uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
       uProgress: new THREE.Uniform(0),
-      uColorA: new THREE.Uniform(new THREE.Color(particles.colorA)),
-      uColorB: new THREE.Uniform(new THREE.Color(particles.colorB))
+      uColorA: new THREE.Uniform(new THREE.Color(particlesAmongUs.colorA)),
+      uColorB: new THREE.Uniform(new THREE.Color(particlesAmongUs.colorB))
     },
     blending: THREE.AdditiveBlending,
     depthWrite: false
-  })
+  });
 
   // Points
-  particles.points = new THREE.Points(particles.geometry, particles.material)
-  particles.points.frustumCulled = false
-  scene.add(particles.points)
+  const pointsAmongUs = new THREE.Points(particlesAmongUs.geometry, particlesAmongUs.material);
+  pointsAmongUs.frustumCulled = false;
+  scene.add(pointsAmongUs);
+});
 
+gltfLoader.load('./rocket.glb', (gltf) => {
+  particlesRocket = {};
+  particlesRocket.index = 0;
+  scene.add(gltf.scene);
 
-  //Methods
-  particles.morph = (index) => {
-    // Update attributes, particles, geometry...
-    particles.geometry.attributes.position = particles.positions[particles.index]
-    particles.geometry.attributes.aPositionTarget = particles.positions[index]
+  // Rocket 모델의 위치를 조정합니다.
+  gltf.scene.position.set(-4, 0, 0); // x, y, z 축으로 이동
 
-    //Animate uProgress
-    gsap.fromTo(
-      particles.material.uniforms.uProgress, {
-        value: 0
-      }, {
-        value: 1,
-        duration: 3,
-        ease: 'linear'
-      }
-    )
+  const positions = gltf.scene.children.map(child => child.geometry.attributes.position);
 
-    //Save index
-    particles.index = index
+  particlesRocket.maxCount = 0;
+
+  for (const position of positions) {
+    if (position.count > particlesRocket.maxCount)
+      particlesRocket.maxCount = position.count;
   }
 
-  particles.morph0 = () => {
-    particles.morph(0)
-  }
-  particles.morph1 = () => {
-    particles.morph(1)
-  }
-  particles.morph2 = () => {
-    particles.morph(2)
-  }
-  particles.morph3 = () => {
-    particles.morph(3)
-  }
+  particlesRocket.positions = positions.map(position => {
+    const originalArray = position.array;
+    const newArray = new Float32Array(particlesRocket.maxCount * 3);
 
-  // 모프 함수 정의
-const morph = (index) => {
-  // 모델 모프 처리
-  particles.morph(index)
-}
+    for (let i = 0; i < particlesRocket.maxCount; i++) {
+      const i3 = i * 3;
 
-// ScrollTrigger을 사용하여 스크롤 위치 감지
-gsap.registerPlugin(ScrollTrigger)
-
-// ScrollTrigger 플러그인 등록
-gsap.registerPlugin(ScrollTrigger);
-
-const sectionRefs = document.querySelectorAll('.section');
-const sections = Array.from(sectionRefs);
-
-// 각 섹션에 대해 ScrollTrigger를 생성하여 모델 모핑
-sections.forEach((section, i) => {
-  ScrollTrigger.create({
-    trigger: section,
-    start: "top bottom-=1",
-    end: "bottom top+=1",
-    onEnter: () => {
-      // 섹션에 진입할 때 모델 모핑
-      const index = sections.indexOf(section);
-      if (index !== -1) {
-        particles.morph(index);
-      }
-    },
-    onEnterBack: () => {
-      // 섹션에 뒤로 돌아갈 때 모델 모핑
-      const index = sections.indexOf(section);
-      if (index !== -1) {
-        particles.morph(index);
+      if (i3 < originalArray.length) {
+        newArray[i3 + 0] = originalArray[i3 + 0];
+        newArray[i3 + 1] = originalArray[i3 + 1];
+        newArray[i3 + 2] = originalArray[i3 + 2];
+      } else {
+        const randomIndex = Math.floor(position.count * Math.random()) * 3;
+        newArray[i3 + 0] = originalArray[randomIndex + 0];
+        newArray[i3 + 1] = originalArray[randomIndex + 1];
+        newArray[i3 + 2] = originalArray[randomIndex + 2];
       }
     }
+
+    return new THREE.Float32BufferAttribute(newArray, 3);
   });
+
+  const sizesArray = new Float32Array(particlesRocket.maxCount);
+
+  for (let i = 0; i < particlesRocket.maxCount; i++)
+    sizesArray[i] = Math.random();
+
+  // Geometry
+  particlesRocket.geometry = new THREE.BufferGeometry();
+  particlesRocket.geometry.setAttribute('position', particlesRocket.positions[particlesRocket.index]);
+  particlesRocket.geometry.setAttribute('aPositionTarget', particlesRocket.positions[0]);
+  particlesRocket.geometry.setAttribute('aSize', new THREE.BufferAttribute(sizesArray, 1));
+
+  // Material
+  particlesRocket.colorA = '#ff7300';
+  particlesRocket.colorB = '#0091ff';
+  particlesRocket.material = new THREE.ShaderMaterial({
+    vertexShader: particlesVertexShader,
+    fragmentShader: particlesFragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(0.4),
+      uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+      uProgress: new THREE.Uniform(0),
+      uColorA: new THREE.Uniform(new THREE.Color(particlesRocket.colorA)),
+      uColorB: new THREE.Uniform(new THREE.Color(particlesRocket.colorB))
+    },
+    blending: THREE.AdditiveBlending,
+    depthWrite: false
+  });
+
+  // Points
+  const pointsRocket = new THREE.Points(particlesRocket.geometry, particlesRocket.material);
+  pointsRocket.frustumCulled = false;
+  scene.add(pointsRocket);
+
 });
-/**
- * Animate
- */
-const tick = () => {
-  // Render normal scene
-  renderer.render(scene, camera);
+//Load models
+// let particles = null
+// gltfLoader.load('./models.glb', (gltf) => {
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-}
+//   /**
+//    * Particles
+//    */
+//   const particles = {}
+//   particles.index = 0
 
-tick();
+//   //Positions
+//   //모델 위치 정보 추출
+//   const positions = gltf.scene.children.map(child =>
+//     child.geometry.attributes.position
+//   )
+
+//   particles.maxCount = 0
+//   //버퍼속성(bufferAttrigute) 에서 최대 정점 수를 찾기위한 코드
+//   for (const position of positions) {
+//     if (position.count > particles.maxCount)
+//       particles.maxCount = position.count
+//   }
+
+//   particles.positions = positions.map( position => {
+//     const originalArray = position.array
+//     const newArray = new Float32Array(particles.maxCount * 3)
+
+//     for (let i = 0; i < particles.maxCount; i++) {
+//       const i3 = i * 3
+
+//       if (i3 < originalArray.length) {
+//         newArray[i3 + 0] = originalArray[i3 + 0]
+//         newArray[i3 + 1] = originalArray[i3 + 1]
+//         newArray[i3 + 2] = originalArray[i3 + 2]
+//       } else {
+//         const randomIndex = Math.floor(position.count * Math.random()) * 3
+//         newArray[i3 + 0] = originalArray[randomIndex + 0]
+//         newArray[i3 + 1] = originalArray[randomIndex + 1]
+//         newArray[i3 + 2] = originalArray[randomIndex + 2]
+//       }
+//     }
+
+//     return new THREE.Float32BufferAttribute(newArray, 3)
+//   })
 
 
-  // Tweaks
-  gui.add(particles.material.uniforms.uProgress, 'value')
-    .min(0).max(1).step(0.001).name('uProgress')
+//   // console.log(particles.positions)
+//   const sizesArray = new Float32Array(particles.maxCount)
 
-  gui.addColor(particles, 'colorA').onChange(() => {
-    particles.material.uniforms.uColorA.value.set(particles.colorA)
-  })
-  gui.addColor(particles, 'colorB').onChange(() => {
-    particles.material.uniforms.uColorB.value.set(particles.colorB)
-  })
+//   for (let i = 0; i < particles.maxCount; i++)
+//     sizesArray[i] = Math.random()
+//   // console.log(sizesArray)
 
-  gui.add(particles, 'morph0')
-  gui.add(particles, 'morph1')
-  gui.add(particles, 'morph2')
-  gui.add(particles, 'morph3')
+//   // Geometry
+//   particles.geometry = new THREE.BufferGeometry()
+//   particles.geometry.setAttribute('position', particles.positions[particles.index])
+//   particles.geometry.setAttribute('aPositionTarget', particles.positions[2])
+//   particles.geometry.setAttribute('aSize', new THREE.BufferAttribute(sizesArray, 1))
+//   //index의 제거방법에대해 기억...
 
-  // Tweaks
-  gui.add(particles.material.uniforms.uProgress, 'value').min(0).max(1).step(0.001).name('uProgress').listen()
 
-})
+//   // Material
+//   particles.colorA = '#ff7300'
+//   particles.colorB = '#0091ff'
+//   particles.material = new THREE.ShaderMaterial({
+//     vertexShader: particlesVertexShader,
+//     fragmentShader: particlesFragmentShader,
+//     uniforms: {
+//       uSize: new THREE.Uniform(0.4),
+//       uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+//       uProgress: new THREE.Uniform(0),
+//       uColorA: new THREE.Uniform(new THREE.Color(particles.colorA)),
+//       uColorB: new THREE.Uniform(new THREE.Color(particles.colorB))
+//     },
+//     blending: THREE.AdditiveBlending,
+//     depthWrite: false
+//   })
+
+//   // Points
+//   particles.points = new THREE.Points(particles.geometry, particles.material)
+//   particles.points.frustumCulled = false
+//   scene.add(particles.points)
+
+
+//   //Methods
+//   particles.morph = (index) => {
+//     // Update attributes, particles, geometry...
+//     particles.geometry.attributes.position = particles.positions[particles.index]
+//     particles.geometry.attributes.aPositionTarget = particles.positions[index]
+
+//     //Animate uProgress
+//     gsap.fromTo(
+//       particles.material.uniforms.uProgress, {
+//         value: 0
+//       }, {
+//         value: 1,
+//         duration: 3,
+//         ease: 'linear'
+//       }
+//     )
+
+//     //Save index
+//     particles.index = index
+//   }
+
+//   particles.morph0 = () => {
+//     particles.morph(0)
+//   }
+//   particles.morph1 = () => {
+//     particles.morph(1)
+//   }
+//   particles.morph2 = () => {
+//     particles.morph(2)
+//   }
+//   particles.morph3 = () => {
+//     particles.morph(3)
+//   }
+
+//   // 모프 함수 정의
+// const morph = (index) => {
+//   // 모델 모프 처리
+//   particles.morph(index)
+// }
+
+
+// // ScrollTrigger 플러그인 등록
+// gsap.registerPlugin(ScrollTrigger);
+
+// const sectionRefs = document.querySelectorAll('.section');
+// const sections = Array.from(sectionRefs);
+
+// // 각 섹션에 대해 ScrollTrigger를 생성하여 모델 모핑
+// sections.forEach((section, i) => {
+//   ScrollTrigger.create({
+//     trigger: section,
+//     start: "top bottom-=1",
+//     end: "bottom top+=1",
+//     scrub: true,
+//     onEnter: () => {
+//       // 섹션에 진입할 때 모델 모핑
+//       const index = sections.indexOf(section);
+//       if (index !== -1) {
+//         particles.morph(index);
+//       }
+//     },
+//     onEnterBack: () => {
+//       // 섹션에 뒤로 돌아갈 때 모델 모핑
+//       const index = sections.indexOf(section);
+//       if (index !== -1) {
+//         particles.morph(index);
+//       }
+//     }
+//   });
+// });
+// /**
+//  * Animate
+//  */
+// const tick = () => {
+//   // Render normal scene
+//   renderer.render(scene, camera);
+
+//   // Call tick again on the next frame
+//   window.requestAnimationFrame(tick);
+// }
+
+// tick();
+
+
+//   // Tweaks
+//   gui.add(particles.material.uniforms.uProgress, 'value')
+//     .min(0).max(1).step(0.001).name('uProgress')
+
+//   gui.addColor(particles, 'colorA').onChange(() => {
+//     particles.material.uniforms.uColorA.value.set(particles.colorA)
+//   })
+//   gui.addColor(particles, 'colorB').onChange(() => {
+//     particles.material.uniforms.uColorB.value.set(particles.colorB)
+//   })
+
+//   gui.add(particles, 'morph0')
+//   gui.add(particles, 'morph1')
+//   gui.add(particles, 'morph2')
+//   gui.add(particles, 'morph3')
+
+//   // Tweaks
+//   gui.add(particles.material.uniforms.uProgress, 'value').min(0).max(1).step(0.001).name('uProgress').listen()
+
+// })
 
 /**
  * Animate
