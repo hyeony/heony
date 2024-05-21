8e31137042febfbea7503dc47ebe82ec92539274
jihyeon.kim <jihyeon.kim@hnine.com>
2024-05-21 18:27:51 +0900

    3dText추가

--- react_gsap/r3f-wawatmos/src/components/Experience.jsx
+++ react_gsap/r3f-wawatmos/src/components/Experience.jsx
@@ -1,15 +1,29 @@
 import { OrbitControls } from "@react-three/drei";
 import { Background } from "./Background";
 
+import { Text3D } from "@react-three/drei";
+
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
       <Background />
-      <mesh>ㄹㄹㄹㄹㄹㄹ
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Text3D
+        font="./fonts/helvetiker_regular.typeface.json"
+        curveSegments={24}
+        brevelSegments={1}
+        bevelEnabled
+        bevelSize={0.08}
+        scale={[1, 1, 1]}
+        height={0.3}
+        lineHeight={0.9}
+        letterSpacing={0.1}
+        color="black" // default
+        anchorX="center" // default
+        anchorY="middle" // default
+      >
+        BOOM & BLOOM
+      </Text3D>
     </>
   );
 };
