7dae305f88fb20173acdb85c6967eadde75126b5
jihyeon.kim <jihyeon.kim@hnine.com>
2024-05-22 17:26:41 +0900

    카메라 각도 테스트

--- react_gsap/r3f-wawatmos/src/App.jsx
+++ react_gsap/r3f-wawatmos/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import Grid3D from "./components/Grid3d";
@@ -6,19 +6,19 @@ import Grid3D from "./components/Grid3d";
 function CameraController() {
   const { camera } = useThree();
   useFrame(() => {
-    camera.lookAt(-3, 11, 9);
+    camera.lookAt(0, 0, 0); // 카메라가 장면의 중심을 바라보도록 설정
   });
   return null;
 }
 
 function App() {
   return (
-    <Canvas camera={{ position: [-5, 20, 15], fov: 40, near: 0.5, far: 1000 }}>
+    <Canvas camera={{ position: [-1.5, 25 , 5], fov: 50, near: 0.1, far: 1000 }}>
       <color attach="background" args={["#ececec"]} />
       <CameraController />
       <Experience />
       <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
+      <pointLight position={[11, 10, 10]} />
       <Grid3D size={30} divisions={5} />
     </Canvas>
   );
