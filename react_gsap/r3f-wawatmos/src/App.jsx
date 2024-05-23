f2b177060465616490e121fa28a91cc94779c427
jihyeon.kim <jihyeon.kim@hnine.com>
2024-05-23 14:24:05 +0900

    카메라 컨트롤러 삭제

--- react_gsap/r3f-wawatmos/src/App.jsx
+++ react_gsap/r3f-wawatmos/src/App.jsx
@@ -4,14 +4,6 @@ import { Experience } from "./components/Experience";
 import { ScrollControls } from "@react-three/drei";
 import Grid3D from "./components/Grid3d";
 
-function CameraController() {
-  const { camera } = useThree();
-  useFrame(() => {
-    camera.lookAt(0, 0, 0); // 카메라가 장면의 중심을 바라보도록 설정
-  });
-  return null;
-}
-
 function App() {
   return (
     <Canvas camera={{ position: [-1.5, 25 , 5], fov: 50, near: 0.1, far: 1000 }}>
@@ -19,7 +11,6 @@ function App() {
       <ambientLight intensity={0.5} />
       <pointLight position={[11, 10, 10]} />
       <ScrollControls pages={3} damping={0.3}>
-        <CameraController />
         <Experience />
         <Grid3D size={30} divisions={5} />
       </ScrollControls>
