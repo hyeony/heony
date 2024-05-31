afd8ced16947d0d687c21747552340cbbb2c15c5
jihyeon.kim <jihyeon.kim@hnine.com>
2024-05-31 10:56:15 +0900

    로고추가

--- react_gsap/r3f-wawatmos/src/App.jsx
+++ react_gsap/r3f-wawatmos/src/App.jsx
@@ -6,15 +6,18 @@ import MovingLights from './components/MovingLight';
 
 function App() {
   return (
-    <Canvas camera={{ position: [-1.5, 15 , 5], fov: 76, near: 0.1, far: 1000 }}>
-      <color attach="background" args={["#ececec"]} />
-      <ambientLight intensity={0.2} />
-      <pointLight position={[11, 10, 10]} />ㄹ고
-      <ScrollControls pages={10} damping={0.4}>
-        <Experience />
-      </ScrollControls>
-      <MovingLights />
-    </Canvas>
+    <>
+      <h1 className="logo">Precise</h1>
+      <Canvas camera={{ position: [-1.5, 15 , 5], fov: 76, near: 0.1, far: 1000 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ambientLight intensity={0.2} />
+        <pointLight position={[11, 10, 10]} />ㄹ고
+        <ScrollControls pages={10} damping={0.4}>
+          <Experience />
+        </ScrollControls>
+        <MovingLights />
+      </Canvas>
+    </>
   );
 }
 
