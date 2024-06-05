e511da76d4319235bfe9f3bf3d85b1ddf4bb5eae
jihyeon.kim <jihyeon.kim@hnine.com>
2024-06-05 16:41:16 +0900

    텍스트 스타일 수정

--- react_gsap/r3f-wawatmos/src/components/Overlay.jsx
+++ react_gsap/r3f-wawatmos/src/components/Overlay.jsx
@@ -23,16 +23,29 @@ export const Overlay = () => {
     <>
       <Scroll html style={{ width: '100%' }}>
         <div className='main'>
-          <div className='main-title main-title-01' style={{ top: `288vh`, left: '6em', transform: `translate(0%,-50%)` }}>
+          <div className='main-title main-title-01' style={{ top: `188vh`,  left: '7vw', }}>
             Beyound Digital Horizon
           </div>
-          <h1 className='main-title main-title-stroke main-title-02' style={{ top: `288vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
+          <h1 className='main-title main-title-stroke main-title-02' style={{ top: `188vh`, left: `7vw` }}>
           BOOMING IDEAS
           </h1>
-          <div className='main-title main-title-03' style={{ top: `311vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
+          <div className='main-title main-title-03' style={{ top: `211vh`, left: `7vw` }}>
             WEB3
           </div>
-          <div className='main-title main-title-stroke main-title-04' style={{ top: `325vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
+          <div className='main-title main-title-stroke main-title-04' style={{ top: `225vh`, left: `7vw` }}>
+            TECHNOLOGY
+          </div>
+
+          <div className='main-title main-title-01' style={{ top: `365vh`, right: '6vw' }}>
+            Beyound Digital Horizon
+          </div>
+          <h1 className='main-title main-title-stroke main-title-02' style={{ top: `362vh`,   right: '6vw' }}>
+          BOOMING IDEAS
+          </h1>
+          <div className='main-title main-title-03' style={{ top: `385vh`,  right: '6vw' }}>
+            WEB3
+          </div>
+          <div className='main-title main-title-stroke main-title-04' style={{ top: `400vh`, right: '6vw' }}>
             TECHNOLOGY
           </div>
         </div>
