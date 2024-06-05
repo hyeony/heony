0549783612b6d610297677a72cbed179590d133e
jihyeon.kim <jihyeon.kim@hnine.com>
2024-06-05 12:18:20 +0900

    공통스타일 변경

--- react_gsap/r3f-wawatmos/src/components/Overlay.jsx
+++ react_gsap/r3f-wawatmos/src/components/Overlay.jsx
@@ -23,16 +23,16 @@ export const Overlay = () => {
     <>
       <Scroll html style={{ width: '100%' }}>
         <div className='main'>
-          <div className='main-title main-title-01' style={{ position: 'absolute', top: `288vh`, left: '6em', transform: `translate(0%,-50%)` }}>
+          <div className='main-title main-title-01' style={{ top: `288vh`, left: '6em', transform: `translate(0%,-50%)` }}>
             Beyound Digital Horizon
           </div>
-          <h1 className='main-title main-title-stroke main-title-02' style={{ position: 'absolute', top: `288vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
+          <h1 className='main-title main-title-stroke main-title-02' style={{ top: `288vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
           BOOMING IDEAS
           </h1>
-          <div className='main-title main-title-03' style={{ position: 'absolute', top: `311vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
+          <div className='main-title main-title-03' style={{ top: `311vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
             WEB3
           </div>
-          <div className='main-title main-title-stroke main-title-04' style={{ position: 'absolute', top: `325vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
+          <div className='main-title main-title-stroke main-title-04' style={{ top: `325vh`, left: '50%', transform: `translate(-40%,-50%)` }}>
             TECHNOLOGY
           </div>
 
